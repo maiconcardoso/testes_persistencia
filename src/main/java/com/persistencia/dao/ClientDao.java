96e32ad4026bccabee6598a834fdf3fa9b052a65
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-01-02 12:46:08 -0300

    Finalização da classe ClientDao

--- src/main/java/com/persistencia/dao/ClientDao.java
+++ src/main/java/com/persistencia/dao/ClientDao.java
@@ -1,8 +1,12 @@
 package com.persistencia.dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Connection;
+
+import java.util.List;
+import java.util.ArrayList;
 
 import com.persistencia.model.Client;
 
@@ -12,10 +16,10 @@ public class ClientDao {
 	Connection con = null;
 	
 	public boolean create(Client custumer) throws ClassNotFoundException, SQLException {
-		String SQL = "INSERT INTO tb_client (name, email, phone) VALUES (?,?,?)";
+		String sql = "INSERT INTO tb_client (name, email, phone) VALUES (?,?,?)";
 		try {
 			con = new ConnectionDatabase().connection();
-			PreparedStatement pstm = con.prepareStatement(SQL);
+			PreparedStatement pstm = con.prepareStatement(sql);
 			pstm.setString(1, custumer.getName());
 			pstm.setString(2, custumer.getEmail());
 			pstm.setString(3, custumer.getPhone());
@@ -28,5 +32,59 @@ public class ClientDao {
 		}
 	}
 	
-	public 
+	public List<Client> readAll() throws SQLException, ClassNotFoundException {
+		String sql = "SELECT * FROM tb_client";
+		List<Client> clientList = new ArrayList<>();
+		try {
+			con = new ConnectionDatabase().connection();
+			PreparedStatement pstm = con.prepareStatement(sql);
+			ResultSet rs = pstm.executeQuery();
+			while(rs.next()) {
+				Client client = new Client();
+				client.setId(rs.getInt("id"));
+				client.setName(rs.getString("name"));
+				client.setEmail(rs.getString("email"));
+				client.setPhone(rs.getString("phone"));
+				clientList.add(client);
+			}
+			return clientList;
+			
+		} catch(SQLException e) {
+			System.err.println(e.getMessage());
+			return null;
+		}
+	}
+	
+	public boolean update(Client custumer) throws SQLException, ClassNotFoundException{
+		String sql = "UPDATE tb_client set name=?, email=?, phone=? where id=?";
+		try {
+			con = new ConnectionDatabase().connection();
+			PreparedStatement pstm = con.prepareStatement(sql);
+			pstm.setString(1, custumer.getName());
+			pstm.setString(2, custumer.getEmail());
+			pstm.setString(3, custumer.getPhone());
+			pstm.setInt(4, custumer.getId());
+			pstm.executeUpdate();
+			return true;
+			
+		} catch(SQLException e) {
+			System.err.println(e.getMessage());
+			return false;
+		}
+	}
+	
+	public boolean delete(Client custumer) throws SQLException, ClassNotFoundException {
+		String sql = "DELETE FROM tb_client WHERE id=?";
+		try {
+			con = new ConnectionDatabase().connection();
+			PreparedStatement pstm = con.prepareStatement(sql);
+			pstm.setInt(1, custumer.getId());
+			pstm.executeUpdate();
+			return true;
+			
+		} catch(SQLException e) {
+			System.err.println(e.getMessage());
+			return false;
+		}
+	}
 }
