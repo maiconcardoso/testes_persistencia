f13b68dfa6adfb96651267c31e410b2fbbe2636c
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-01-02 13:26:32 -0300

    Alteração dos dados para teste

--- src/main/java/com/persistencia/controller/client/InsertClient.java
+++ src/main/java/com/persistencia/controller/client/InsertClient.java
@@ -8,7 +8,7 @@ import com.persistencia.model.Client;
 public class InsertClient {
 	
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
-		Client custumer = new Client("Frodo Bolseiro", "frodobolseiro@gmail.com", "(44) 3046-1003");
+		Client custumer = new Client("Meriadoc Brandebuque", "merlin@gmail.com", "(44) 3058-1011");
 		ClientDao custumerDao = new ClientDao();
 		custumerDao.create(custumer);
 	}
